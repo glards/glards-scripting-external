a82abf1463726d7c22b47375d866d15fcac29f81
Glards <sglardon@gmail.com>
2022-07-02 14:51:38 +0200

    Working dotnet server

--- src/server/dotnet/DotnetGrpcServer/Program.cs
+++ src/server/dotnet/DotnetGrpcServer/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Security.Authentication;
+using DotnetGrpcServer.Services;
+using Microsoft.AspNetCore.HttpLogging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//builder.WebHost.ConfigureKestrel(kestrel =>
+//{
+//    kestrel.ConfigureHttpsDefaults(https =>
+//    {
+//        https.SslProtocols = SslProtocols.Tls12;
+//        https.AllowAnyClientCertificate();
+//    });
+//});
+
+
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+//builder.Services.AddHttpLogging(opt =>
+//{
+//    opt.LoggingFields = HttpLoggingFields.Request;
+//});
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+//app.UseHttpLogging();
+// Configure the HTTP request pipeline.
+app.MapGrpcService<ExternalScriptingService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
---
+++ src/server/dotnet/DotnetGrpcServer/Services/ExternalScriptingService.cs
@@ -0,0 +1,14 @@
+﻿using Externalscripting;
+using Grpc.Core;
+
+namespace DotnetGrpcServer.Services
+{
+    public class ExternalScriptingService : Externalscripting.ExternalScripting.ExternalScriptingBase
+    {
+        public override Task<EventResponse> TriggerEvent(EventData request, ServerCallContext context)
+        {
+            Console.WriteLine($"> Received event : {request.EventName}");
+            return Task.FromResult<EventResponse>(new EventResponse());
+        }
+    }
+}
